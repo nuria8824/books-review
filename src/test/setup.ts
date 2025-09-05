2bfbc67d9d922162ebbde81fd7f8cd6e4b0b6e90
Nuria Desteffani <nuria.desteffani@uap.edu.ar>
2025-09-05 05:59:51 -0300

    Unit testing de auth

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,16 @@
 import '@testing-library/jest-dom'
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+let mongod: MongoMemoryServer;
+
+export async function setupTestDB() {
+  mongod = await MongoMemoryServer.create();
+  const uri = mongod.getUri();
+  await mongoose.connect(uri);
+}
+
+export async function teardownTestDB() {
+  await mongoose.disconnect();
+  await mongod.stop();
+}
